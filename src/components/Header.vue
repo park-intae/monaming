92c65f4f7805b04c123ac457b194e9ca4236bd99
DESKTOP-VJ6LN2T\ParkIntae <pit1995@outlook.kr>
2025-04-03 23:56:02 +0900

    Header template 완료 이제 Footer template랑 대략적인 script만들어야 됨

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -28,7 +28,36 @@
             </div>
         </div>
     </div>
-    <div id="header_menu"></div>
+    <div id="header_menu">
+        <nav>
+            <div id="logo"></div>
+            <div id="menu">
+                <ul>
+                    <li v-for="(item, index) in mainMenu" :key="index">
+                        <a :href="item.link">{{ item.name }}</a>
+                        <ul v-if="item.subMenu && item.subMenu.length > 0">
+                            <li v-for="(subItem, subIndex) in item.subMenu" :key="subIndex">
+                                <a :href="subItem.link">{{ subItem.name }}</a>
+                            </li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+            <div id="toolbar">
+                <div id="search">
+                    <input type="text" />
+                    <a>
+                        <img src="/assets/btn_search.gif" alt="search" />
+                    </a>
+                </div>
+                <div id="all_btn">
+                    <div class="all_btn_ele"></div>
+                    <div class="all_btn_ele"></div>
+                    <div class="all_btn_ele"></div>
+                </div>
+            </div>
+        </nav>
+    </div>
 </template>
 
 <script>
@@ -37,6 +66,67 @@ export default {
     name: 'Header',
     data() {
         return {
+            mainMenu: [
+                {
+                    name: '모나미소개',
+                    link: '#',
+                    subMenu: [
+                        { name: "CEO 메세지", link: '#' },
+                        { name: "회사정보", link: '#' },
+                        { name: "회사연혁", link: '#' },
+                        { name: "윤리강령", link: '#' },
+                        { name: "투자정보", link: '#' },
+                        { name: "CI", link: '#' },
+                        { name: "찾아오시는 길", link: '#' },
+                        { name: "안전보건 경영방침", link: '#' },
+                    ]
+                },
+                {
+                    name: '모나미제품',
+                    link: '#',
+                    subMenu: [
+                        { name: "프리미엄 펜", link: '#' },
+                        { name: "펜·펜슬", link: '#' },
+                        { name: "마카·컬러링", link: '#' },
+                        { name: "노트·사무용품", link: '#' },
+                        { name: "잉크·리필", link: '#' },
+                        { name: "카탈로그", link: '#' },
+                    ]
+                },
+                {
+                    name: 'NEWS & VIDEO',
+                    link: '#',
+                    subMenu: [
+                        { name: "보도자료", link: '#' },
+                        { name: "동영상자료", link: '#' },
+                    ]
+                },
+                {
+                    name: '모나미 미술대회',
+                    link: '#',
+                    subMenu: [
+                        { name: "미술대회 소개", link: '#' },
+                        { name: "수상작 발표", link: '#' },
+                    ]
+                },
+                {
+                    name: '고객지원',
+                    link: '#',
+                    subMenu: [
+                        { name: "자주묻는질문", link: '#' },
+                        { name: "문의하기", link: '#' },
+                    ]
+                },
+                {
+                    name: '기업 구매 / 개발',
+                    link: '#',
+                    subMenu: [
+                        { name: "대량 구매 문의", link: '#' },
+                        { name: "기업 및 단체 구매", link: '#' },
+                        { name: "산업용 맞춤 개발발", link: '#' },
+                    ]
+                },
+            ],
             sns: [
                 { name: 'Facebook', icon: '/assets/sns_facebook.gif', link: '#' },
                 { name: 'Instagram', icon: '/assets/sns_insta.gif', link: '#' },
